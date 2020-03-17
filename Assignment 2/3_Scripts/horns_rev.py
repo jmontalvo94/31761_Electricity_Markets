64a95ca842c57657b2f4050e293017df91108740
jmontalvo94 <50923223+jmontalvo94@users.noreply.github.com>
2020-03-17 22:08:52 +0100

    Update horns_rev.py

--- Assignment 2/3_Scripts/horns_rev.py
+++ Assignment 2/3_Scripts/horns_rev.py
@@ -5,11 +5,13 @@ Created on Tue Mar 17 02:17:29 2020
 @author: jorge
 """
 # Import libraries
-import urllib.request
+import pandas as pd
 
 # Fetch data from URL
-req = urllib.request.Request('http://pierrepinson.com/31761/Assignments/windpowerforecasts.dat')
-with urllib.request.urlopen(req) as response:
-   data = response.read()
-   
+rawdata_url = 'http://pierrepinson.com/31761/Assignments/windpowerforecasts.dat'
+df = pd.read_csv(rawdata_url, sep=';')
+
+# Count NaN
+df.isna().sum()
+
 #%%
