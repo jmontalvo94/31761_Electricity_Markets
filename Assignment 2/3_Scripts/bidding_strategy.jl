c642f5ec87a32929c3ddf7ff27c6c8ad5e58948c
Jorge Montalvo <jorge_ma94@hotmail.com>
2020-04-10 15:20:17 +0200

    Update assignment 2

--- Assignment 2/3_Scripts/bidding_strategy.jl
+++ Assignment 2/3_Scripts/bidding_strategy.jl
@@ -1,34 +1,61 @@
 ## Import packages
 
+using CSV
 using DataFrames
 using Dates
 using DelimitedFiles
+using HTTP
 
 ## Define functions
 
 function get_file_from(url::String, missing_str::String)
     file = HTTP.get(url)
-    buffer = IOBuffer(file.body)
-    df = CSV.read(buffer, missingstring=missing_str)
+    df = DataFrame(CSV.File(file.body; missingstring = missing_str))
     return df
 end
 
-function replace_string!(df::DataFrame, weird::String, actual::Int64,
-    cols::Tuple{Int64,Int64,Int64})
+function replace_string!(
+    df::DataFrame,
+    weird_str::String,
+    normal_str::String,
+    cols::Tuple{Integer,Integer,Integer}
+)
     for col in cols
-        df[!,col] = replace(df[!,col], weird => actual)
+        for (i, str) in enumerate(df[!,col])
+            if str == weird_str
+                df[i,col] = normal_str
+            end
+        end
+    end
+    return df
+end
+
+function convert_str_to_int!(df::DataFrame, cols::Tuple{Integer,Integer,Integer})
+    for col in cols
+        df[!,col] = parse.(Int64, df_forecast[:,col])
     end
 end
 
-## Load raw data
+function convert_int_to_date!(df::DataFrame, cols::Tuple{Integer,Integer})
+    format = DateFormat("yyyymmddHHMM")
+    for col in cols
+        df[!,col] = DateTime.(string.(df[!,col]), format)
+    end
+end
+
+
+## Load data and clean
 
 # Fetch forecast data from URL
 forecast_url = "http://pierrepinson.com/31761/Assignments/windpowerforecasts.dat"
 df_forecast = get_file_from(forecast_url, "NA")
 
-# Drop missing values and replace "1e+05" to 100000,
-dropmissing!(df_forecast; disallowmissing=true)
-replace_string!(df_forecast, "1e+05", 100000, (6,16,24))
+# Drop rows with missing values
+dropmissing!(df_forecast)
 
-# Define data types and convert columns
-forecast_types = reduce(vcat,(fill(Date,2),fill(Int64,22)))
+# Replace string "1e+05" to "100000"
+replace_string!(df_forecast, "1e+05", "100000", (6,16,24))
+
+# Convert types
+convert_str_to_int!(df_forecast, (6,16,24))
+convert_int_to_date!(df_forecast, (1,2))
