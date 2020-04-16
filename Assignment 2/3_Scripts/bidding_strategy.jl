2e20448c1f05701813cf23119f12d4970558c9dd
Jorge Montalvo <jorge_ma94@hotmail.com>
2020-04-16 02:17:56 +0200

    Update script

--- Assignment 2/3_Scripts/bidding_strategy.jl
+++ Assignment 2/3_Scripts/bidding_strategy.jl
@@ -21,9 +21,9 @@ function replace_string!(
     cols::Tuple{Integer,Integer,Integer}
 )
     for col in cols
-        for (i, str) in enumerate(df[!,col])
+        for (i, str) in enumerate(df[:, col])
             if str == weird_str
-                df[i,col] = normal_str
+                df[i, col] = normal_str
             end
         end
     end
@@ -32,7 +32,7 @@ end
 
 function convert_str_to_int!(df::DataFrame, cols::Tuple{Integer,Integer,Integer})
     for col in cols
-        df[!,col] = parse.(Int64, df_forecast[:,col])
+        df[!, col] = parse.(Int64, df_forecast[:, col])
     end
     return df
 end
@@ -40,7 +40,7 @@ end
 function convert_int_to_date!(df::DataFrame, cols::Tuple{Integer,Integer})
     format = DateFormat("yyyymmddHHMM")
     for col in cols
-        df[!,col] = DateTime.(string.(df[!,col]), format)
+        df[!, col] = DateTime.(string.(df[:, col]), format)
     end
     return df
 end
@@ -49,12 +49,12 @@ function read_to_dfs(files::Array{String,1})
 	dfs = Array{DataFrame,1}(undef,4)
 	for (i, file) in enumerate(files)
 	    if file[1:6] == "elspot"
-			df = DataFrame(CSV.File(file, header=3, datarow=4))
+			df = DataFrame(CSV.File(file; header=3, datarow=4))
 		    df = rename!(df, :1 => "Date")
 	        df = select!(df, [:Date, :Hours, :DK1])
 			dfs[i] = df
 		else
-			df = DataFrame(CSV.File(file, header=4, datarow=5))
+			df = DataFrame(CSV.File(file; header=4, datarow=5))
 		    df = rename!(df, :1 => "Date")
 	        df = select!(df, :Date, :Hours, :23, :24)
 			dfs[i] = df
@@ -63,67 +63,117 @@ function read_to_dfs(files::Array{String,1})
 	return dfs[1], dfs[2], dfs[3], dfs[4]
 end
 
+function adjust_DST_March!(dfs::Array{DataFrame,1})
+	for df in dfs
+		missing_idx = findfirst(ismissing, df[!, end])
+		df[missing_idx, end] = df[missing_idx + 1, end]
+		if length(names(df)) > 3
+			df[missing_idx, end - 1] = df[missing_idx + 1, end - 1]
+		end
+		dropmissing!(df)
+	end
+	return dfs
+end
+
+function adjust_DST_October!(dfs::Array{DataFrame,1})
+	for df in dfs
+		unique!(df, [:Date, :Hours])
+	end
+	return dfs
+end
+
 function adjust_date!(dfs::Array{DataFrame,1})
 	for df in dfs
-		newDate = fill(Date("1994-01-01"), size(df, 1))
-	    for (i, oldDate) in enumerate(df.Date)
+		date_new = fill(Date("1994-01-01"), size(df, 1))
+	    for (i, date_old) in enumerate(df.Date)
 			try
-				newDate[i] = Date(oldDate, "dd/mm/yyyy")
+				date_new[i] = Date(date_old, "dd/mm/yyyy")
 			catch
-				newDate[i] = Date(oldDate, "dd-mm-yyyy")
+				date_new[i] = Date(date_old, "dd-mm-yyyy")
 			end
 		end
-		select!(df, Not(:Date)) # Remove old date
-		insertcols!(df, 1, :Date => newDate) # Insert new date
-		insertcols!(df, 2, :Year => year.(newDate))
-		insertcols!(df, 3, :Month => month.(newDate))
-		insertcols!(df, 4, :Day => day.(newDate))
+		df.Date = date_new
 	end
+	return dfs
 end
 
 function adjust_hours!(dfs::Array{DataFrame,1})
 	for df in dfs
-		newHour = fill(0, size(df.Hours,1))
-		for (i, oldHour) in enumerate(df.Hours)
-			newHour[i] = parse(Int64, oldHour[1:2]) + 1
+		hours_new = fill(0, size(df.Hours, 1))
+		for (i, hours_old) in enumerate(df.Hours)
+			hours_new[i] = parse(Int64, hours_old[1:2]) + 1
 		end
-		select!(df, Not(:Hours)) # Remove old hour
-		insertcols!(df, 5, :Hour => newHour) # Insert new hour
+		df.Hours = hours_new
 	end
+	return dfs
 end
 
+function convert_str_to_int!(dfs::Array{DataFrame,1})
+    for df in dfs
+		temp = replace.(df[:, end], "," => ".")
+        df[!, end] = parse.(Float64, temp)
+		if length(names(df)) > 3
+			temp = replace.(df[:, end - 1], "," => ".")
+			df[!, end - 1] = parse.(Float64, temp)
+		end
+    end
+    return dfs
+end
 
-## Load data and clean
+## Data loading and cleaning
 
-# Fetch forecast data from URL
+# Fetch forecast data from URL and change column name for q10
 forecast_url = "http://pierrepinson.com/31761/Assignments/windpowerforecasts.dat"
 df_forecast = get_file_from(forecast_url, "NA")
+rename!(df_forecast, Symbol("10") => :q10)
 
-# Cleaning
+#=
+Missing values:
+There are 122 missing actual power measurements and 238 missing deterministic power
+forecasts and quantiles; these are removed.
+=#
+df_missing_meas = filter(row -> ismissing(row[:meas]), df_forecast)
+df_missing_rest = filter(row -> ismissing(row[:fore]), df_forecast)
 dropmissing!(df_forecast)
-replace_string!(df_forecast, "1e+05", "100000", (6,16,24))
-convert_str_to_int!(df_forecast, (6,16,24))
+
+#=
+Dataset conversions:
+There are four "1e+05" values in the dataset that are saved as Strings; these are changed
+and the columns converted to Int. Then dato and dati are converted to DateTime.
+=#
+if (sum(String .== eltype.(eachcol(df_forecast))) > 0) # If at least 1 String column type
+	replace_string!(df_forecast, "1e+05", "100000", (6,16,24))
+	convert_str_to_int!(df_forecast, (6,16,24))
+end
 convert_int_to_date!(df_forecast, (1,2))
 
-# File names to be fetched
+# Fetch market data from CSVs and load to dfs
 files = ["elspot-prices_2016_hourly_eur.csv","elspot-prices_2017_hourly_eur.csv",
 	"regulating-prices_2016_hourly_eur.csv","regulating-prices_2017_hourly_eur.csv"]
-
-# Read files into separate DataFrames
 df_elspot16, df_elspot17, df_reg16, df_reg17 = read_to_dfs(files)
 
-# Check for missing
-for df in [df_elspot16, df_elspot17]
-	println(filter(row -> ismissing(row[:DK1]), df))
-end
-for df in [df_reg16, df_reg17]
-	println(filter(row -> ismissing(row[:Up_10]), df))
-	println(filter(row -> ismissing(row[:Down_10]), df))
-end
+#=
+DST timezone change:
+There's a missing value when there's a DST change at hours 2 - 3 of March 26, 2016 and
+March 27, 2017; the value of the next hour 3 - 4 is copied instead of the missing value.
+Also, there's a repeted value with the reverse change at hours 2 - 3 of October 30, 2016
+and October 29, 2017; one of the repeated dates was removed from the dataset.
+=#
+adjust_DST_March!([df_elspot16, df_elspot17, df_reg16, df_reg17])
+adjust_DST_October!([df_elspot16, df_elspot17, df_reg16, df_reg17])
 
-# Cleaning
+#=
+Dataset conversions:
+Column Date of each DataFrame is converted to Date type, and columns Hours, Up_10, and
+Down_10 are converted to Integer.
+=#
 adjust_date!([df_elspot16, df_elspot17, df_reg16, df_reg17])
 adjust_hours!([df_elspot16, df_elspot17, df_reg16, df_reg17])
-for df in [df_elspot16, df_elspot17, df_reg16, df_reg17]
-	dropmissing!(df)
-end
+convert_str_to_int!([df_elspot16, df_elspot17, df_reg16, df_reg17])
+
+
+## Analysis
+
+# Create clean datetime indexes to compare with datasets
+dt_16 = collect(DateTime(2016,1,1,0,0,0):Hour(1):DateTime(2016,12,31,23,0,0))
+dt_17 = collect(DateTime(2017,1,1,0,0,0):Hour(1):DateTime(2017,12,31,23,0,0))
